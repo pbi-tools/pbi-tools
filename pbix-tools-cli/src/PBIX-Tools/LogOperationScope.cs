fe228d27049d320ccb17beefc609cbfef88aa7b2
Mathias Thierbach <mt-dev@outlook.com>
2018-03-24 11:33:59 +0000

    Created vscode worspace with sub-folders for cli and vscode-ext

---
+++ pbix-tools-cli/src/PBIX-Tools/LogOperationScope.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using Serilog;
+using Serilog.Events;
+
+namespace PbixTools
+{
+    public class LogOperationScope : IDisposable
+    {
+        private readonly Stopwatch _stopWatch = Stopwatch.StartNew();
+        private readonly string _label;
+        private readonly LogEventLevel _logLevel;
+        private readonly ILogger _log;
+
+        /// <summary>
+        /// Creates a new <see cref="LogOperationScope"/>.
+        /// </summary>
+        /// <param name="label">A custom label to include in log messages.</param>
+        /// <param name="logInfo"><c>True</c> to log at Information level, otherwise at Debug.</param>
+        /// <param name="log">A specific <see cref="ILogger"/> to use. If not specified, uses the globally-shared instance.</param>
+        public LogOperationScope(string label, bool logInfo = true, ILogger log = null)
+        {
+            this._label = label;
+            this._logLevel = logInfo ? LogEventLevel.Information : LogEventLevel.Debug;
+            this._log = log ?? Log.Logger;
+
+            if (_log.IsEnabled(_logLevel)) _log.Write(_logLevel, "Starting operation: {Operation}", _label);
+        }
+
+        void IDisposable.Dispose()
+        {
+            if (_log.IsEnabled(_logLevel)) _log.Write(_logLevel, "Operation completed: {Operation}. Took {Elapsed}.", _label, _stopWatch.Elapsed);
+        }
+    }
+}
