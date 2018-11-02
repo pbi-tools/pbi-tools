7e6e7e9cdca0a62d0e3cb16b1e087c1ed1117d48
Mathias Thierbach <mt-dev@outlook.com>
2018-11-02 07:49:47 +0000

    CLI build script cleanup. Had started out with Paket's script @ https://github.com/fsprojects/Paket/blob/master/build.fsx

--- pbi-tools-cli/build.fsx
+++ pbi-tools-cli/build.fsx
@@ -17,8 +17,6 @@ open Fake.AssemblyInfoFile
 open Fake.ReleaseNotesHelper
 open System
 open System.IO
-open Fake.Testing.NUnit3
-open System.Security.Cryptography
 
 // --------------------------------------------------------------------------------------
 // START TODO: Provide project-specific details below
@@ -75,8 +73,6 @@ let buildDir = ".build"
 let outDir = buildDir @@ "out"
 let distDir = buildDir @@ "dist"
 let tempDir = ".temp"
-let buildMergedDir = buildDir @@ "merged"
-let paketFile = buildMergedDir @@ "paket.exe"
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
@@ -104,21 +100,6 @@ let stable =
     | _ -> release
 
 
-let genFSAssemblyInfo (projectPath) =
-    let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
-    let folderName = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(projectPath))
-    let basePath = "src" @@ folderName
-    let fileName = basePath @@ "AssemblyInfo.fs"
-    CreateFSharpAssemblyInfo fileName
-      [ Attribute.Title (projectName)
-        Attribute.Product project
-        Attribute.Company (authors |> String.concat ", ")
-        Attribute.Copyright (sprintf "Copyright \u00A9 Mathias Thierbach %i" 2018)
-        Attribute.Description summary
-        Attribute.Version release.AssemblyVersion
-        Attribute.FileVersion fileVersion
-        Attribute.InformationalVersion release.NugetVersion ]
-
 let genCSAssemblyInfo (projectPath) =
     let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
     let folderName = System.IO.Path.GetDirectoryName(projectPath)
@@ -136,9 +117,7 @@ let genCSAssemblyInfo (projectPath) =
 
 // Generate assembly info files with the right version & up-to-date information
 Target "AssemblyInfo" (fun _ ->
-    let fsProjs =  !! "src/**/*.fsproj" |> Seq.filter (fun s -> not <| s.Contains("preview"))
     let csProjs = !! "src/**/*.csproj" |> Seq.filter (fun s -> not <| s.Contains("preview"))
-    fsProjs |> Seq.iter genFSAssemblyInfo
     csProjs |> Seq.iter genCSAssemblyInfo
 )
 
@@ -160,9 +139,6 @@ Target "Clean" (fun _ ->
     |> DeleteFiles
 )
 
-Target "CleanDocs" (fun _ ->
-    CleanDirs ["docs/output"]
-)
 
 // --------------------------------------------------------------------------------------
 // Build library & test project
@@ -185,360 +161,15 @@ Target "Build" (fun _ ->
     |> CopyFiles distDir
 )
 
-let assertExitCodeZero x = 
-    if x = 0 then () else 
-    failwithf "Command failed with exit code %i" x
-
-let runCmdIn workDir exe = 
-    Printf.ksprintf (fun args -> 
-        tracefn "%s %s" exe args
-        Shell.Exec(exe, args, workDir) |> assertExitCodeZero)
-
-
-
-// --------------------------------------------------------------------------------------
-// Run the unit tests using test runner
-
-Target "RunTests" (fun _ ->
-    CreateDir "tests_result/net/Paket.Tests"
-
-    !! testAssemblies
-    |> NUnit3 (fun p ->
-        { p with
-            ShadowCopy = false
-            WorkingDir = "tests_result/net/Paket.Tests" |> Path.GetFullPath
-            Agents = Some 1 //workaround for https://github.com/nunit/nunit-console/issues/219 
-            TimeOut = TimeSpan.FromMinutes 20. })
-)
-
-
-
-// --------------------------------------------------------------------------------------
-// Build a NuGet package
-
-let mergeLibs = ["paket.exe"; "Paket.Core.dll"; "FSharp.Core.dll"; "Newtonsoft.Json.dll"; "Argu.dll"; "Chessie.dll"; "Mono.Cecil.dll"]
-
-Target "MergePaketTool" (fun _ ->
-    CreateDir buildMergedDir
-    
-    let toPack =
-        mergeLibs
-        |> List.map (fun l -> buildDir @@ l)
-        |> separated " "
-
-    let result =
-        ExecProcess (fun info ->
-            info.FileName <- currentDirectory </> "packages" </> "build" </> "ILRepack" </> "tools" </> "ILRepack.exe"
-            info.Arguments <- sprintf "/lib:%s /ver:%s /out:%s %s" buildDir release.AssemblyVersion paketFile toPack
-            ) (TimeSpan.FromMinutes 5.)
-
-    if result <> 0 then failwithf "Error during ILRepack execution."
-)
-
-
-
-
-Target "CalculateDownloadHash" (fun _ ->
-    use stream = File.OpenRead(paketFile)
-    use sha = new SHA256Managed()
-    let checksum = sha.ComputeHash(stream)
-    let hash = BitConverter.ToString(checksum).Replace("-", String.Empty)
-    File.WriteAllText(buildMergedDir @@ "paket-sha256.txt", sprintf "%s paket.exe" hash)
-)
-
-Target "NuGet" (fun _ ->
-    let testTemplateFiles =
-        !! "integrationtests/**/paket.template"
-        |> Seq.map (fun f -> f, f + "-disabled")
-        |> Seq.toList
-        
-
-    try
-        testTemplateFiles
-        |> Seq.iter (fun (f, d) -> File.Move(f, d))
-
-        let files = !! "src/**/*.preview*" |> Seq.toList
-        for file in files do
-            File.Move(file,file + ".temp")
-
-        Paket.Pack (fun p -> 
-            { p with 
-                ToolPath = "bin/merged/paket.exe" 
-                Version = release.NugetVersion
-                ReleaseNotes = toLines release.Notes })
-
-        for file in files do
-            File.Move(file + ".temp",file)
-    finally
-        testTemplateFiles
-        |> Seq.iter (fun (f, d) ->
-            if File.Exists(d) then File.Move(d, f))
-)
-
-
-
-Target "PublishNuGet" (fun _ ->
-    if hasBuildParam "PublishBootstrapper" |> not then
-        !! (tempDir </> "*bootstrapper*")
-        |> Seq.iter File.Delete
-
-    !! (tempDir </> "dotnetcore" </> "*.nupkg")
-    |> Seq.iter File.Delete
-
-    Paket.Push (fun p -> 
-        { p with 
-            ToolPath = "bin/merged/paket.exe"
-            WorkingDir = tempDir }) 
-)
-
-
-// --------------------------------------------------------------------------------------
-// Generate the documentation
-
-let disableDocs = true // https://github.com/fsprojects/FSharp.Formatting/issues/461
-
-let fakePath = "packages" @@ "build" @@ "FAKE" @@ "tools" @@ "FAKE.exe"
-let fakeStartInfo fsiargs script workingDirectory args environmentVars =
-    (fun (info: System.Diagnostics.ProcessStartInfo) ->
-        info.FileName <- fakePath
-        info.Arguments <- sprintf "%s --fsiargs %s -d:FAKE \"%s\"" args fsiargs script
-        info.WorkingDirectory <- workingDirectory
-        let setVar k v =
-            info.EnvironmentVariables.[k] <- v
-        for (k, v) in environmentVars do
-            setVar k v
-        setVar "MSBuild" msBuildExe
-        setVar "GIT" Git.CommandHelper.gitPath
-        setVar "FSI" fsiPath)
-
-
-/// Run the given startinfo by printing the output (live)
-let executeWithOutput configStartInfo =
-    let exitCode =
-        ExecProcessWithLambdas
-            configStartInfo
-            TimeSpan.MaxValue false ignore ignore
-    System.Threading.Thread.Sleep 1000
-    exitCode
-
-/// Run the given startinfo by redirecting the output (live)
-let executeWithRedirect errorF messageF configStartInfo =
-    let exitCode =
-        ExecProcessWithLambdas
-            configStartInfo
-            TimeSpan.MaxValue true errorF messageF
-    System.Threading.Thread.Sleep 1000
-    exitCode
-
-/// Helper to fail when the exitcode is <> 0
-let executeHelper executer fail traceMsg failMessage configStartInfo =
-    trace traceMsg
-    let exit = executer configStartInfo
-    if exit <> 0 then
-        if fail then
-            failwith failMessage
-        else
-            traceImportant failMessage
-    else
-        traceImportant "Succeeded"
-    ()
-
-let execute = executeHelper executeWithOutput
-
-Target "GenerateReferenceDocs" (fun _ ->
-    if disableDocs then () else
-    let args = ["--define:RELEASE"; "--define:REFERENCE"]
-    let argLine = System.String.Join(" ", args)
-    execute
-      true
-      (sprintf "Building reference documentation, this could take some time, please wait...")
-      "generating reference documentation failed"
-      (fakeStartInfo argLine "generate.fsx" "docs/tools" "" [])
-)
-
-
-
-
-let generateHelp' commands fail debug =
-    // remove FSharp.Compiler.Service.MSBuild.v12.dll
-    // otherwise FCS thinks  it should use msbuild, which leads to insanity
-    !! "packages/**/FSharp.Compiler.Service.MSBuild.*.dll"
-    |> DeleteFiles
-
-    let args =
-        [ if not debug then yield "--define:RELEASE"
-          if commands then yield "--define:COMMANDS"
-          yield "--define:HELP"]
-    let argLine = System.String.Join(" ", args)
-    execute
-      fail
-      (sprintf "Building documentation (%A), this could take some time, please wait..." commands)
-      "generating documentation failed"
-      (fakeStartInfo argLine "generate.fsx" "docs/tools" "" [])
-
-    CleanDir "docs/output/commands"
-
-let generateHelp commands fail =
-    generateHelp' commands fail false
-
-Target "GenerateHelp" (fun _ ->
-    if disableDocs then () else
-    DeleteFile "docs/content/release-notes.md"
-    CopyFile "docs/content/" "RELEASE_NOTES.md"
-    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
-
-    DeleteFile "docs/content/license.md"
-    CopyFile "docs/content/" "LICENSE.txt"
-    Rename "docs/content/license.md" "docs/content/LICENSE.txt"
-
-    generateHelp true true
-)
-
-Target "GenerateHelpDebug" (fun _ ->
-    if disableDocs then () else
-    DeleteFile "docs/content/release-notes.md"
-    CopyFile "docs/content/" "RELEASE_NOTES.md"
-    Rename "docs/content/release-notes.md" "docs/content/RELEASE_NOTES.md"
-
-    DeleteFile "docs/content/license.md"
-    CopyFile "docs/content/" "LICENSE.txt"
-    Rename "docs/content/license.md" "docs/content/LICENSE.txt"
-
-    generateHelp' true true true
-)
-
-Target "KeepRunning" (fun _ ->    
-    use watcher = !! "docs/content/**/*.*" |> WatchChanges (fun changes ->
-         generateHelp false false
-    )
-
-    traceImportant "Waiting for help edits. Press any key to stop."
-
-    System.Console.ReadKey() |> ignore
-
-    watcher.Dispose()
-)
-
-Target "GenerateDocs" DoNothing
-
-// --------------------------------------------------------------------------------------
-// Release Scripts
-
-Target "ReleaseDocs" (fun _ ->
-    if disableDocs then () else   
-    let tempDocsDir = "temp/gh-pages"
-    CleanDir tempDocsDir
-    Repository.cloneSingleBranch "" (gitHome + "/" + gitName + ".git") "gh-pages" tempDocsDir
-
-    Git.CommandHelper.runSimpleGitCommand tempDocsDir "rm . -f -r" |> ignore
-    CopyRecursive "docs/output" tempDocsDir true |> tracefn "%A"
-
-    File.WriteAllText("temp/gh-pages/latest",sprintf "https://github.com/fsprojects/Paket/releases/download/%s/paket.exe" release.NugetVersion)
-    File.WriteAllText("temp/gh-pages/stable",sprintf "https://github.com/fsprojects/Paket/releases/download/%s/paket.exe" stable.NugetVersion)
-
-    StageAll tempDocsDir
-    Git.Commit.Commit tempDocsDir (sprintf "Update generated documentation for version %s" release.NugetVersion)
-    Branches.push tempDocsDir
-)
-
-#load "paket-files/build/fsharp/FAKE/modules/Octokit/Octokit.fsx"
-open Octokit
-
-Target "ReleaseGitHub" (fun _ ->
-    let user =
-        match getBuildParam "github_user" with
-        | s when not (String.IsNullOrWhiteSpace s) -> s
-        | _ ->
-            eprintfn "Please update your release script to set 'github_user'!"
-            match getBuildParam "github-user" with
-            | s when not (String.IsNullOrWhiteSpace s) -> s
-            | _ -> getUserInput "Username: "
-    let pw =
-        match getBuildParam "github_password" with
-        | s when not (String.IsNullOrWhiteSpace s) -> s
-        | _ ->
-            eprintfn "Please update your release script to set 'github_password'!"
-            match getBuildParam "github_pw", getBuildParam "github-pw" with
-            | s, _ | _, s when not (String.IsNullOrWhiteSpace s) -> s
-            | _ -> getUserPassword "Password: "
-    let remote =
-        Git.CommandHelper.getGitResult "" "remote -v"
-        |> Seq.filter (fun (s: string) -> s.EndsWith("(push)"))
-        |> Seq.tryFind (fun (s: string) -> s.Contains(gitOwner + "/" + gitName))
-        |> function None -> gitHome + "/" + gitName | Some (s: string) -> s.Split().[0]
-
-    StageAll ""
-    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
-    Branches.pushBranch "" remote (Information.getBranchName "")
-
-    Branches.tag "" release.NugetVersion
-    Branches.pushTag "" remote release.NugetVersion
-    
-    // release on github
-    createClient user pw
-    |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes 
-    |> uploadFile "./bin/merged/paket.exe"
-    |> uploadFile "./bin/merged/paket-sha256.txt"
-    |> uploadFile "./bin/paket.bootstrapper.exe"
-    |> uploadFile ".paket/paket.targets"
-    |> uploadFile ".paket/Paket.Restore.targets"
-    |> releaseDraft
-    |> Async.RunSynchronously
-)
-
-
-Target "Release" DoNothing
-Target "BuildPackage" DoNothing
-Target "BuildCore" DoNothing
 Target "Help" DoNothing
+
 // --------------------------------------------------------------------------------------
-// Run all targets by default. Invoke 'build <Target>' to override
-
-Target "All" DoNothing
-
-"Clean"
-  ==> "BuildCore"
 
 "Clean"
   ==> "AssemblyInfo"
   ==> "Build"
-  <=> "BuildCore"
-  ==> "RunTests"
-  =?> ("GenerateReferenceDocs",isLocalBuild && not isMono && not (hasBuildParam "SkipDocs"))
-  =?> ("GenerateDocs",isLocalBuild && not isMono && not (hasBuildParam "SkipDocs"))
-  ==> "All"
-  =?> ("ReleaseDocs",isLocalBuild && not isMono && not (hasBuildParam "SkipDocs"))
 
-"All"
-  ==> "MergePaketTool"
-  ==> "CalculateDownloadHash"
-  =?> ("NuGet", not <| hasBuildParam "SkipNuGet")
-  ==> "BuildPackage"
-
-
-
-"CleanDocs"
-  ==> "GenerateHelp"
-  ==> "GenerateReferenceDocs"
-  ==> "GenerateDocs"
-
-"CleanDocs"
-  ==> "GenerateHelpDebug"
-
-"GenerateHelp"
-  ==> "KeepRunning"
-
-"BuildPackage"
-  ==> "PublishNuGet"
-
-"PublishNuGet"
-  ==> "ReleaseGitHub"
-  ==> "Release"
-
-"ReleaseGitHub"
-  ?=> "ReleaseDocs"
-
-"ReleaseDocs"
-  ==> "Release"
+// --------------------------------------------------------------------------------------
+// Show help by default. Invoke 'build <Target>' to override
 
 RunTargetOrDefault "Help"
