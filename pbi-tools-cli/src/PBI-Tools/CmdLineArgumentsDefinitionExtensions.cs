5b6635aea8cae00642da162f276a90de0b424975
Mathias Thierbach <mt-dev@outlook.com>
2020-04-26 20:43:16 +0100

    Added {C} note

--- pbi-tools-cli/src/PBI-Tools/CmdLineArgumentsDefinitionExtensions.cs
+++ pbi-tools-cli/src/PBI-Tools/CmdLineArgumentsDefinitionExtensions.cs
@@ -1,3 +1,6 @@
+// Copyright (c) Mathias Thierbach
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
