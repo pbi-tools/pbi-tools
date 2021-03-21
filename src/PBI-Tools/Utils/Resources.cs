69b34cb8153d28bc7ee986a7f1077fb57ca12952
Mathias Thierbach <mt-dev@outlook.com>
2021-03-21 23:10:37 +0000

    Added various overloads in Resources utility class

--- src/PBI-Tools/Utils/Resources.cs
+++ src/PBI-Tools/Utils/Resources.cs
@@ -5,24 +5,42 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace PbiTools.Utils
 {
     public static class Resources
     {
-        public static T GetEmbeddedResource<T>(string name, Func<Stream, T> transform)
+        public static T GetEmbeddedResource<T>(string name, Func<Stream, T> transform, Assembly assembly = null)
         {
-            var asm = Assembly.GetCallingAssembly();
-
-            using (var stream = GetEmbeddedResourceStream(name, asm))
+            using (var stream = GetEmbeddedResourceStream(name, assembly ?? Assembly.GetCallingAssembly()))
             {
                 return transform(stream);
             }
         }
 
+        public static string GetEmbeddedResourceString(string name, Encoding encoding = null, Assembly assembly = null)
+        {
+            using (var stream = GetEmbeddedResourceStream(name, assembly ?? Assembly.GetCallingAssembly()))
+            using (var reader = new StreamReader(stream, encoding ?? Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static T GetEmbeddedResourceFromString<T>(string name, Func<string, T> transform, Assembly assembly = null) =>
+            GetEmbeddedResource<T>(name, stream =>
+            {
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    return transform(reader.ReadToEnd());
+                }
+            }, assembly ?? Assembly.GetCallingAssembly());
+
         public static Stream GetEmbeddedResourceStream(string name, Assembly assembly = null)
         {
             var asm = assembly ?? Assembly.GetCallingAssembly();
+
             var resourceNames = asm.GetManifestResourceNames();
             var match = resourceNames.FirstOrDefault(n => n.EndsWith(name));
             if (match == null) throw new ArgumentException($"Embedded resource '{name}' not found.", nameof(name));
