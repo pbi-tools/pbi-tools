e8cc4ab8754751cb14ef75c3c14968ad1bee311b
Mathias Thierbach <mt-dev@outlook.com>
2023-03-29 23:09:38 +0100

    - #195 Set (Cloud) credentials during dataset deployment - Anonymous - #195 Set (Cloud) credentials during dataset deployment - OAuth2 - #195 Case-insensitive matching of data sources - Manifest Schema Changes   - manifest.credentials[].updateMode: { NotSpecified, Always, Never, BeforeRefresh }   - manifest.credentials[].type: { Basic, Anonymous, OAuth2 }   - manifest.credentials[].authority   - manifest.credentials[].validateAuthority   - manifest.credentials[].tenantId   - manifest.credentials[].clientId   - manifest.credentials[].clientSecret   - manifest.credentials[].scopes   - manifest.credentials[].useDeploymentToken

--- src/PBI-Tools/Deployments/DatasetCredentialsManager.cs
+++ src/PBI-Tools/Deployments/DatasetCredentialsManager.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.Identity.Client;
 using Microsoft.PowerBI.Api;
 using Microsoft.PowerBI.Api.Models;
 using Microsoft.PowerBI.Api.Models.Credentials;
@@ -21,12 +22,16 @@ namespace PbiTools.Deployments
 
         private readonly PbiDeploymentCredential[] _credentials;
         private readonly IPowerBIClient _powerBI;
+        private readonly bool _refreshEnabled;
+        private readonly AuthenticationResult _deploymentToken;
 
-        public DatasetCredentialsManager(PbiDeploymentManifest manifest, IPowerBIClient powerBIClient)
+        public DatasetCredentialsManager(PbiDeploymentManifest manifest, IPowerBIClient powerBIClient, bool refreshEnabled, AuthenticationResult deploymentToken)
         {
             _credentials = (manifest ?? throw new ArgumentNullException(nameof(manifest))).Credentials;
             _powerBI = powerBIClient ?? throw new ArgumentNullException(nameof(powerBIClient));
-
+            _refreshEnabled = refreshEnabled;
+            _deploymentToken = deploymentToken;
+            
             Enabled = manifest.Options.Dataset.SetCredentials;
         }
 
@@ -69,25 +74,23 @@ namespace PbiTools.Deployments
                         continue;
                     }
 
-                    if (credential.Type != CredentialType.Basic)
-                        throw new NotSupportedException("Only 'Basic' credentials are currently supported.");
+                    if (credential.UpdateMode == PbiDeploymentCredential.CredentialUpdateMode.BeforeRefresh && !_refreshEnabled) {
+                        Log.Information("- Skipping datasource because UpdateMode is set to 'BeforeRefresh' and refresh is disabled.");
+                        continue;
+                    }
 
-                    var credentialDetails = new CredentialDetails(
-                        new BasicCredentials(
-                            credential.Username.ExpandEnv(), 
-                            credential.Password.ExpandEnv()
-                        )
-                        , credential.Details.PrivacyLevel
-                        , credential.Details.EncryptedConnection
-                    )
-                    { 
-                        EncryptionAlgorithm = credential.Details.EncryptionAlgorithm,
-                        UseCallerAADIdentity = credential.Details.UseCallerAADIdentity,
-                        UseEndUserOAuth2Credentials = credential.Details.UseEndUserOAuth2Credentials
+                    var credentialDetails = credential.Type switch
+                    {
+                        var type when type == CredentialType.Basic     => GetBasicCredentials(credential),
+                        var type when type == CredentialType.Anonymous => GetAnonymousCredentials(credential),
+                        var type when type == CredentialType.OAuth2    => await GetOAuthCredentials(credential),
+                        _ => throw new NotSupportedException($"CredentialType {credential.Type} is not supported.")
                     };
 
+                    // TODO Grab DM_ErrorDetailNameCode_UnderlyingErrorMessage from result if error
                     await _powerBI.Gateways.UpdateDatasourceAsync(
-                        result.GatewayId.Value, result.DatasourceId.Value, 
+                        result.GatewayId.Value,
+                        result.DatasourceId.Value,
                         new() { CredentialDetails = credentialDetails }
                     );
 
@@ -96,23 +99,81 @@ namespace PbiTools.Deployments
                         credential.Match.ConnectionDetails.ToJsonString(Newtonsoft.Json.Formatting.None));
 
                 }
-
             }
 
+        }
+
+        private static CredentialDetails GetBasicCredentials(PbiDeploymentCredential credential) =>
+            new (new BasicCredentials(
+                    credential.Username.ExpandEnv(),
+                    credential.Password.ExpandEnv()
+                )
+                , credential.Details.PrivacyLevel
+                , credential.Details.EncryptedConnection
+            )
+            {
+                EncryptionAlgorithm = credential.Details.EncryptionAlgorithm,
+                UseCallerAADIdentity = credential.Details.UseCallerAADIdentity,
+                UseEndUserOAuth2Credentials = credential.Details.UseEndUserOAuth2Credentials
+            };
+
+        private static CredentialDetails GetAnonymousCredentials(PbiDeploymentCredential credential) =>
+            new (new AnonymousCredentials()
+                , credential.Details.PrivacyLevel
+                , EncryptedConnection.NotEncrypted
+            );
+
+        private async Task<CredentialDetails> GetOAuthCredentials(PbiDeploymentCredential credential)
+        {
+            var token = _deploymentToken;
+
+            if (!credential.UseDeploymentToken)
+            {
+                var tokenProvider = new ServicePrincipalTokenProvider(credential.ExpandAndValidate());
+                token = await tokenProvider.AcquireTokenAsync();
+
+                Log.Information("Received AccessToken from endpoint: {TokenEndpoint}; Expires On: {ExpiresOn}"
+                    , token.AuthenticationResultMetadata.TokenEndpoint
+                    , token.ExpiresOn);
+            }
+
+            return new CredentialDetails(
+                new OAuth2Credentials(token.AccessToken)
+                , credential.Details.PrivacyLevel
+                , credential.Details.EncryptedConnection
+            )
+            {
+                EncryptionAlgorithm = credential.Details.EncryptionAlgorithm,
+            };
         }
 
         private static PropertyInfo[] ConnectionDetailsProperties = typeof(DatasourceConnectionDetails).GetProperties();
 
         internal static bool TryGetMatch(PbiDeploymentCredential.CredentialMatch match, IEnumerable<Datasource> dataSources, out Datasource matchedDataSource)
         {
-            var matchValues = ConnectionDetailsProperties.ToDictionary(x => x.Name, y => y.GetValue(match.ConnectionDetails));
+            var matchValues = ConnectionDetailsProperties.ToDictionary(
+                x => x.Name,
+                y => (y.GetValue(match.ConnectionDetails) switch {
+                    string s => s,
+                    null => "",
+                    var o => o.ToString()
+                }).ToLowerInvariant()
+            );
 
             foreach (var dataSource in dataSources)
             {
-                if (dataSource.DatasourceType.Equals(match.DatasourceType, StringComparison.InvariantCultureIgnoreCase)) { 
-                    var dataSourceValues = ConnectionDetailsProperties.ToDictionary(x => x.Name, y => y.GetValue(dataSource.ConnectionDetails));
+                if (dataSource.DatasourceType.Equals(match.DatasourceType, StringComparison.InvariantCultureIgnoreCase))
+                { 
+                    var dataSourceValues = ConnectionDetailsProperties.ToDictionary(
+                        x => x.Name, 
+                        y => (y.GetValue(dataSource.ConnectionDetails) switch {
+                            string s => s,
+                            null => "",
+                            var o => o.ToString()
+                        }).ToLowerInvariant()
+                    );
 
-                    var mismatches = matchValues.Except(dataSourceValues /* TODO Consider weaker EqualityComparer */);
+                    var mismatches = matchValues.Except(dataSourceValues);
                     if (mismatches.Count() == 0) {
                         matchedDataSource = dataSource;
                         return true;
--- src/PBI-Tools/Deployments/ServicePrincipalTokenProvider.cs
+++ src/PBI-Tools/Deployments/ServicePrincipalTokenProvider.cs
@@ -7,32 +7,44 @@ using Microsoft.Identity.Client;
 
 namespace PbiTools.Deployments
 {
-    public interface IPowerBITokenProvider
+    public interface IOAuthTokenProvider
     {
         Task<AuthenticationResult> AcquireTokenAsync();
     }
 
-    public class ServicePrincipalPowerBITokenProvider : IPowerBITokenProvider
+    public class ServicePrincipalTokenProvider : IOAuthTokenProvider
     {
-        public const string POWERBI_API_RESOURCE = "https://analysis.windows.net/powerbi/api";
-        private static readonly string[] scopes = new [] { $"{POWERBI_API_RESOURCE}/.default" };
         private readonly IConfidentialClientApplication _app;
+        private readonly string[] scopes;
 
-
-        public ServicePrincipalPowerBITokenProvider(PbiDeploymentAuthentication options)
+        public ServicePrincipalTokenProvider(PbiDeploymentOAuthCredentials options, params string[] scopes)
         {
             var builder = ConfidentialClientApplicationBuilder
                 .Create(options.ClientId)
                 .WithClientSecret(options.ClientSecret);
-                // TODO Support Certificate
+            // TODO Support Certificate
 
             _app = (options.Authority != null
                 ? builder.WithAuthority(options.Authority, options.ValidateAuthority)
                 : builder.WithTenantId(options.TenantId)
             ).Build();
+
+            this.scopes = options.Scopes == null || options.Scopes.Length == 0
+                ? scopes
+                : options.Scopes;
         }
-        
+
         public Task<AuthenticationResult> AcquireTokenAsync() => _app.AcquireTokenForClient(scopes).ExecuteAsync();
+    }
+
+    public class PowerBIServicePrincipalTokenProvider : ServicePrincipalTokenProvider
+    {
+        public const string POWERBI_API_RESOURCE = "https://analysis.windows.net/powerbi/api";
+
+        public PowerBIServicePrincipalTokenProvider(PbiDeploymentOAuthCredentials options)
+            : base(options, $"{POWERBI_API_RESOURCE}/.default")
+        { }
+        
     }
 
 }
