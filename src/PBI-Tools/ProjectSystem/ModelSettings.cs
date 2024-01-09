15d3553c65872eca51b8c7eedb5901359fc76747
Mathias Thierbach <mt-dev@outlook.com>
2024-01-09 19:28:06 +0000

    Support for TMDL MetadataSerializationOptions via PbixProj - PbixProj v0.14 - #296 TMDL Serializer now honors settings.model.annotations

--- src/PBI-Tools/ProjectSystem/ModelSettings.cs
+++ src/PBI-Tools/ProjectSystem/ModelSettings.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Runtime.Serialization;
+using Microsoft.AnalysisServices.Tabular.Serialization;
 using Newtonsoft.Json;
 using PowerArgs;
 
@@ -17,7 +18,7 @@ namespace PbiTools.ProjectSystem
         Raw = 2,
         [ArgDescription("Serializes the tabular model into the default PbixProj folder structure and performs various transformations to optimize file contents for source control.")]
         Legacy = 3,
-        [ArgDescription("Serializes the tabular model into TMDL format.")]
+        [ArgDescription("Serializes the tabular model into TMDL format. Annotation settings are applied.")]
         Tmdl = 4,
 
         // [ArgDescription("Serializes the tabular model into a single JSON file containing the full TMSL payload from the PBIX model. All transformations are applied.")]
@@ -47,7 +48,9 @@ namespace PbiTools.ProjectSystem
         public bool IsDefault => 
             _ignoreProperties == null
             && SerializationMode == ModelSerializationMode.Default
-            && Annotations.IsDefault();
+            && Annotations.IsDefault()
+            && Formatting.IsDefault()
+            && Measures.IsDefault();
 
         #region IgnoreProperties
 
@@ -80,12 +83,31 @@ namespace PbiTools.ProjectSystem
         [JsonProperty("measures", NullValueHandling = NullValueHandling.Ignore)]
         public ModelMeasureSettings Measures { get; set; } = new();
 
+        /// <summary>
+        /// Defines formatting options for the TMDL serializer.
+        /// </summary>
+        [JsonProperty("formatting", NullValueHandling = NullValueHandling.Ignore)]
+        public ModelFormattingSettings Formatting { get; set; } = new();
+
+        /// <summary>
+        /// If set, only the root object will be serialized.
+        /// </summary>
+        [JsonProperty("excludeChildrenMetadata", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? ExcludeChildrenMetadata { get; set; }
+
+        /// <summary>
+        /// If set, restricted information is included when serializing.
+        /// </summary>
+        [JsonProperty("includeRestrictedInformation", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? IncludeRestrictedInformation { get; set; }
+
 
 #region Json Serialization Support
         [OnSerializing]
         private void HideDefaultValuesOnSerializing(StreamingContext context)
         {
             if (Annotations.IsDefault()) Annotations = null;
+            if (Formatting.IsDefault()) Formatting = null;
             if (Measures.IsDefault()) Measures = null;
         }
 
@@ -93,9 +115,49 @@ namespace PbiTools.ProjectSystem
         private void ResetDefaultValuesAfterSerializing(StreamingContext context)
         {
             if (Annotations == null) Annotations = new();
+            if (Formatting == null) Formatting = new();
             if (Measures == null) Measures = new();
         }
 #endregion
+    }
+
+    public class ModelFormattingSettings : IHasDefaultValue
+    {
+        /// <summary>
+        /// Sets the encoding to use when serializing to TMDL. Default: 'utf-8'.
+        /// </summary>
+        [JsonProperty("encoding", NullValueHandling = NullValueHandling.Ignore)]
+        public string Encoding { get; set; } = System.Text.Encoding.UTF8.BodyName;
+
+        internal System.Text.Encoding GetEncoding() => System.Text.Encoding.GetEncoding(Encoding);
+
+        /// <summary>
+        /// Controls how line breaks are emitted.
+        /// Supported values are <c>SystemDefault</c>, <c>WindowsStyle</c>, <c>UnixStyle</c>.
+        /// </summary>
+        [JsonProperty("newLineStyle", NullValueHandling = NullValueHandling.Ignore)]
+        public NewLineStyle NewLineStyle { get; set; } = default;
+
+        /// <summary>
+        /// Controls the indentation mode.
+        /// Supported values are <c>Default</c>, <c>Tabs</c>, <c>Spaces</c>.
+        /// </summary>
+        [JsonProperty("indentationMode", NullValueHandling = NullValueHandling.Ignore)]
+        public IndentationMode IndentationMode { get; set; } = default;
+
+        /// <summary>
+        /// A whole number defining the number of columns used for each indentation level.
+        /// </summary>
+        [JsonProperty("indentationSize", NullValueHandling = NullValueHandling.Ignore)]
+        public int IndentationSize { get; set; } = 2;
+
+        [JsonIgnore]
+        public bool IsDefault =>
+            Encoding == System.Text.Encoding.UTF8.BodyName
+            && NewLineStyle == default
+            && IndentationMode == default
+            && IndentationSize == 2;
+
     }
 
     public class ModelAnnotationSettings : IHasDefaultValue
