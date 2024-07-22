22379bb21e6505c7ad9d15fcc14cc34272305d42
Mathias Thierbach <mt-dev@outlook.com>
2024-07-22 10:57:57 +0100

    Updated all file license headers * new build target: "WriteHeaders"

--- src/PBI-Tools/Win32/SystemUtility.cs
+++ src/PBI-Tools/Win32/SystemUtility.cs
@@ -1,4 +1,22 @@
-﻿// Attribution: https://github.com/projectkudu/KuduHandles/tree/8c34ac5/KuduHandles
+/*
+ * This file is part of the pbi-tools project <https://github.com/pbi-tools/pbi-tools>.
+ * Copyright (C) 2018 Mathias Thierbach
+ *
+ * pbi-tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pbi-tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * A copy of the GNU Affero General Public License is available in the LICENSE file,
+ * and at <https://goto.pbi.tools/license>.
+ */
+
+/** Attribution: https://github.com/projectkudu/KuduHandles/tree/8c34ac5/KuduHandles */
 
 #if NETFRAMEWORK
 using System;
